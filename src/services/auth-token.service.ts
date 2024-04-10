28d614914bf9ba139d376912595febf4fe6f3e7b
Oleksandr <oleksandrzelenjak@gmail.com>
2024-04-10 23:34:41 +0300

    fixed

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'plotterhub.site',
+		domain: 'https://plotterhub.site',
 		sameSite: 'strict',
 		expires: 1
 	})
