d2d008b44056ef1df705ef4677c2e23356815c4d
Oleksandr <oleksandrzelenjak@gmail.com>
2024-04-11 00:01:43 +0300

    fixed import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
   useEffect(() => {
     router.push('/i');
-  }, [router]);
+  }, []);
 
 	return 
 }
--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'https://plotterhub.site',
+		domain: 'plotterhub.site',
 		sameSite: 'strict',
 		expires: 1
 	})
